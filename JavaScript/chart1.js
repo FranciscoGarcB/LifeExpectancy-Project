18b6c4802c1028099966a56e6f16f31007e52619
Francisco García <franciscogarcb@outlook.com>
2023-12-15 20:01:40 +0100

    Added files

--- JavaScript/chart1.js
+++ JavaScript/chart1.js
@@ -2,11 +2,11 @@ var tooltip = d3.select("#tooltip");
 
 // Load countries GeoJSON files
 d3.json('datasets/countries.geojson').then(function(geojsonData) {
-    // Selecciona el elemento donde se mostrará el mapa
+    // Select the element where the map will be displayed
     var container = d3.select("#chart1");
     var svg = container.append("svg");
 
-    // Función para actualizar el tamaño y la proyección del mapa
+    // Function to update the size and projection of the map
     function updateMapSize() {
         var width = container.node().getBoundingClientRect().width;
         var height = container.node().getBoundingClientRect().height;
@@ -14,7 +14,7 @@ d3.json('datasets/countries.geojson').then(function(geojsonData) {
         svg.attr("width", width).attr("height", height);
 
         var projection = d3.geoNaturalEarth1()
-            .scale(width / 6.5) // Ajusta la escala según el ancho
+            .scale(width / 5.5) // Adjust scale according to width
             .translate([width / 2, height / 2]);
 
         var path = d3.geoPath().projection(projection);
@@ -24,90 +24,177 @@ d3.json('datasets/countries.geojson').then(function(geojsonData) {
             .enter().append("path")
             .attr("d", path)
             .attr("class", "country");
-            
-}
+    }
 
+    // Call updateMapSize initially and when the window size changes
+    updateMapSize();
+    window.addEventListener("resize", updateMapSize);
 
-// Llama a updateMapSize inicialmente y cuando cambie el tamaño de la ventana
-updateMapSize();
-window.addEventListener("resize", updateMapSize);
+    var legendHeight = 150; // Height of the legend
+    var legendWidth = 20;
+    var legendMargin = { left: 20, top: 20, right: 5, bottom: 5 };
 
-// Cargar el archivo CSV y actualizar el mapa según el año seleccionado
-d3.csv('datasets/LifeExpectancyUpdated.csv').then(function(lifeExpectancyData) {
-    function updateMapForYear(selectedYear) {
-        // Filtrar los datos por el año seleccionado
-        var filteredData = lifeExpectancyData.filter(function(d) {
-            return d.Year == selectedYear;
+    // Position the legend inside the SVG
+    var legendXPosition = legendMargin.left;
+    var legendYPosition = legendMargin.top;
+
+    // Create the legend
+    var legend = svg.append("g")
+        .attr("id", "legend")
+        .attr("transform", "translate(" + legendXPosition + "," + legendYPosition + ")");
+
+    // Define a scale for the legend
+    var legendScale = d3.scaleLinear()
+        .range([legendHeight, 0]);
+
+    // Add the axis to the legend (without scale for now)
+    var legendAxis = d3.axisRight(legendScale);
+    legend.append("g")
+        .attr("transform", "translate(" + legendWidth + ",0)")
+        .attr("class", "legend-axis");
+
+    var lifeExpectancyDataByCountry = {};
+
+    d3.csv('datasets/LifeExpectancyUpdated.csv').then(function(lifeExpectancyData) {
+        lifeExpectancyData.forEach(function(d) {
+            if (!lifeExpectancyDataByCountry[d.Country]) {
+                lifeExpectancyDataByCountry[d.Country] = [];
+            }
+            lifeExpectancyDataByCountry[d.Country].push({ year: +d.Year, value: +d['Life expectancy '] });
         });
+    });
 
-        // Crear un mapa de los datos filtrados para un acceso rápido por país
-        var lifeExpectancyByCountry = {};
-        filteredData.forEach(function(d) {
-            lifeExpectancyByCountry[d.Country] = +d['Life expectancy ']; // Asegúrate de que el nombre de la columna coincida exactamente
-        });
+    function drawLineChart(data) {
+        var margin = { top: 15, right: 15, bottom: 20, left: 30 },
+            width = 150 - margin.left - margin.right,
+            height = 150 - margin.top - margin.bottom;
 
-        // Crear una escala de colores
-        var colorScale = d3.scaleSequential(d3.interpolateBlues)
-            .domain(d3.extent(filteredData, function(d) { return +d['Life expectancy ']; }));
+        var svg = d3.create("svg")
+            .attr("viewBox", `0 0 ${width + margin.left + margin.right} ${height + margin.top + margin.bottom}`);
 
-        // Actualizar los colores de los países en el mapa
-        svg.selectAll("path.country")
-            .attr("fill", function(d) {
-                return lifeExpectancyByCountry[d.properties.name] ? colorScale(lifeExpectancyByCountry[d.properties.name]) : '#ccc';
-            })
-            .on("mouseover", function(event, d) {
-                tooltip.style("display", "block")
-                    .html(d.properties.name)
-                    .style("left", (event.pageX + 10) + "px")
-                    .style("top", (event.pageY + 10) + "px");
-            })
-            .on("mouseout", function() {
-                tooltip.style("display", "none");
+        var x = d3.scaleLinear()
+            .domain(d3.extent(data, function(d) { return d.year; }))
+            .range([0, width]);
+
+        var y = d3.scaleLinear()
+            .domain([35, d3.max(data, function(d) { return d.value; })])
+            .range([height, 0]);
+
+        var line = d3.line()
+            .x(function(d) { return x(d.year); })
+            .y(function(d) { return y(d.value); });
+
+        svg.append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+            .append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr("stroke-width", 1.5)
+            .attr("d", line);
+
+        // Add axes to the chart
+        var xAxis = d3.axisBottom(x).ticks(4).tickFormat(d3.format("d"));
+        var yAxis = d3.axisLeft(y).ticks(3);
+
+        svg.append("g")
+            .attr("transform", "translate(" + margin.left + "," + (height + margin.top) + ")")
+            .call(xAxis);
+
+        svg.append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+            .call(yAxis);
+
+        return svg.node();
+    }
+
+    // Load the CSV file and update the map based on the selected year
+    d3.csv('datasets/LifeExpectancyUpdated.csv').then(function(lifeExpectancyData) {
+        function updateMapForYear(selectedYear) {
+            // Filter data by the selected year
+            var filteredData = lifeExpectancyData.filter(function(d) {
+                return d.Year == selectedYear;
             });
 
-        // Crear la leyenda
-        var legendHeight = 200, legendWidth = 20, margin = 10;
-        var legend = svg.append("g")
-            .attr("id", "legend")
-            .attr("transform", "translate(" + (container.node().getBoundingClientRect().width - legendWidth - margin) + "," + margin + ")");
+            // Create a map of filtered data for quick access by country
+            var lifeExpectancyByCountry = {};
+            filteredData.forEach(function(d) {
+                lifeExpectancyByCountry[d.Country] = +d['Life expectancy ']; // Make sure the column name matches exactly
+            });
 
-        // Definir una escala para la leyenda
-        var legendScale = d3.scaleLinear()
-            .domain(colorScale.domain())
-            .range([legendHeight, 0]);
+            // Create a color scale
+            var colorScale = d3.scaleSequential(d3.interpolateBlues)
+                .domain(d3.extent(filteredData, function(d) { return +d['Life expectancy ']; }));
 
-        // Definir el eje para la leyenda
-        var legendAxis = d3.axisRight(legendScale)
-            .ticks(5); // Ajusta según sea necesario
+            // Update the colors of countries on the map
+            svg.selectAll("path.country")
+                .transition() // Inicia la transición
+        .       duration(200) // Duración de la transición en milisegundos
+                .attr("fill", function(d) {
+                    var countryName = d.properties.name;
+                    var lifeExpectancy = lifeExpectancyByCountry[countryName];
+                    return lifeExpectancy ? colorScale(lifeExpectancy) : '#ccc';
+                })
 
-        // Dibujar la leyenda
-        legend.selectAll("rect")
-            .data(colorScale.ticks().map(function(d, i, ticks) {
-                return {
-                    y0: i / ticks.length * legendHeight,
-                    y1: (i + 1) / ticks.length * legendHeight,
-                    z: d
-                };
-            }))
-            .enter().append("rect")
+            // Update the colors of countries on the map
+            svg.selectAll("path.country")
+                .on("mouseover", function(event, d) {
+                    var countryName = d.properties.name;
+                    var lifeExpectancy = lifeExpectancyByCountry[countryName];
+                    var tooltipText = countryName;
+
+                    if (lifeExpectancy) {
+                        tooltipText += ": " + lifeExpectancy.toFixed(2) + " years";
+                    } else {
+                        tooltipText += ": No data";
+                    }
+
+                    var lineChartData = lifeExpectancyDataByCountry[countryName];
+                    if (lineChartData) {
+                        var lineChart = drawLineChart(lineChartData);
+                        tooltip.html(tooltipText)
+                            .style("display", "block")
+                            .style("left", (event.pageX + 10) + "px")
+                            .style("top", (event.pageY + 10) + "px")
+                            .node().appendChild(lineChart);
+                    }
+                })
+                .on("mouseout", function() {
+                    tooltip.style("display", "none").html("");
+                });
+
+            // Update the legend scale
+            legendScale.domain(d3.extent(filteredData, function(d) { return +d['Life expectancy ']; }));
+
+            // Invert the way legend rectangles' values are calculated
+            var legendRects = legend.selectAll("rect")
+                .data(colorScale.ticks().map(function(d, i, ticks) {
+                    return {
+                        y0: legendHeight - (i + 1) / ticks.length * legendHeight,
+                        y1: legendHeight - i / ticks.length * legendHeight,
+                        z: d
+                    };
+                }));
+
+            legendRects.enter().append("rect")
+                .merge(legendRects)
                 .attr("y", function(d) { return d.y0; })
                 .attr("height", function(d) { return d.y1 - d.y0; })
                 .attr("width", legendWidth)
                 .style("fill", function(d) { return colorScale(d.z); });
 
-        // Añadir el eje a la leyenda
-        legend.append("g")
-            .attr("transform", "translate(" + legendWidth + ",0)")
-            .call(legendAxis);
-            
+            legendRects.exit().remove();
+
+            // Update the legend axis
+            legend.select(".legend-axis").call(legendAxis);
         }
 
-    // Escuchar cambios en el control deslizante de año
-    document.getElementById('yearRange').addEventListener('input', function() {
-        updateMapForYear(this.value);
-    });
+        // Listen for changes in the year slider
+        document.getElementById('yearRange').addEventListener('input', function() {
+            updateMapForYear(this.value);
+        });
 
-    // Actualizar inicialmente con el valor por defecto del control deslizante
-    updateMapForYear(document.getElementById('yearRange').value);
-});
+        // Initially update with the default value of the slider
+        updateMapForYear(document.getElementById('yearRange').value);
+    });
 });
---
+++ JavaScript/chart2.js
@@ -0,0 +1,158 @@
+var tooltip = d3.select("#tooltip");
+
+var svg;
+var x, y;
+var currentFilter = getTop10; // Inicializar con Top 10
+
+// Definir márgenes en un ámbito global
+var margin = {top: 20, right: 130, bottom: 20, left: 150};
+
+function getWidth() {
+    var containerWidth = document.getElementById('chart2').getBoundingClientRect().width;
+    return containerWidth - margin.left - margin.right;
+}
+
+function initializeChart() {
+    var width = getWidth();
+    var height = 2100 - margin.top - margin.bottom; // Altura inicial para Top 10 y Bottom 10
+
+    svg = d3.select("#chart2")
+        .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    x = d3.scaleLinear().range([0, width]);
+    y = d3.scaleBand().range([0, height]).padding(.1);
+
+    svg.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", "translate(0," + height + ")");
+    svg.append("g")
+        .attr("class", "y-axis");
+}
+
+function updateBarChart(selectedYear) {
+    d3.csv('../datasets/LifeExpectancyShort.csv').then(function(data) {
+        var width = getWidth();
+
+        var filteredData = currentFilter(data.filter(d => d.Year == selectedYear));
+
+        // Ajustar la altura y estilo del contenedor
+        var height = (currentFilter === getAllData) ? 2100 : 400;
+        height -= margin.top + margin.bottom;
+
+        // Ajustar el tamaño del SVG y la escala Y
+        svg.attr("height", height + margin.top + margin.bottom);
+        y.range([0, height]);
+        svg.select(".x-axis").attr("transform", "translate(0," + height + ")");
+
+        // Actualizar escalas
+        x.range([0, width]);
+        x.domain([0, d3.max(filteredData, d => +d['Life expectancy '])]);
+        y.domain(filteredData.map(d => d.Country));
+
+        // Actualizar los ejes
+        svg.select(".x-axis").call(d3.axisBottom(x));
+        svg.select(".y-axis").call(d3.axisLeft(y));
+
+        // Seleccionar y actualizar las barras
+        var bars = svg.selectAll("rect")
+            .data(filteredData, d => d.Country);
+
+        // Transición para barras existentes
+        bars.transition()
+            .duration(800)
+            .attr("y", d => y(d.Country))
+            .attr("width", d => x(d['Life expectancy ']))
+            .attr("height", y.bandwidth()); // Actualizar la altura
+
+        // Agregar nuevas barras
+        bars.enter().append("rect")
+            .attr("x", x(0))
+            .attr("y", d => y(d.Country))
+            .attr("height", y.bandwidth()) // Establecer la altura
+            .attr("fill", "#3081D0")
+            .transition()
+            .duration(800)
+            .attr("width", d => x(d['Life expectancy ']));
+
+        // Eliminar barras que ya no se necesitan
+        bars.exit().remove();
+
+        // Eventos del mouse
+        svg.selectAll("rect")
+            .on("mouseover", function(event, d) {
+                tooltip.style("display", "block");
+                tooltip.html("Country: " + d.Country + "<br/>Life Expectancy: " + d['Life expectancy '])
+                    .style("left", (event.pageX + 10) + "px")
+                    .style("top", (event.pageY + 10) + "px");
+                d3.select(this).attr("fill", "#F4F27E");
+            })
+            .on("mousemove", function(event) {
+                tooltip.style("left", (event.pageX + 10) + "px")
+                    .style("top", (event.pageY + 10) + "px");
+            })
+            .on("mouseout", function() {
+                tooltip.style("display", "none");
+                d3.select(this).attr("fill", "#3081D0");
+            });
+    });
+
+    // Aplicar estilo scrolleable si el filtro es "Show All"
+    var chartContainer = document.getElementById('chart2');
+    if (currentFilter === getAllData) {
+        chartContainer.classList.add('scrollable');
+    } else {
+        chartContainer.classList.remove('scrollable');
+        chartContainer.style.maxHeight = null; // Restablecer la altura máxima
+    }
+}
+
+// Funciones de Filtrado
+function getTop10(data) {
+    return data.sort((a, b) => b['Life expectancy '] - a['Life expectancy ']).slice(0, 10);
+}
+
+function getBottom10(data) {
+    return data.sort((a, b) => a['Life expectancy '] - b['Life expectancy ']).slice(0, 10);
+}
+
+function getAllData(data) {
+    return data;
+}
+
+// Manejadores de eventos para radio buttons
+document.querySelectorAll('input[name="filter"]').forEach(radio => {
+    radio.addEventListener('change', function() {
+        switch(this.value) {
+            case 'top10':
+                currentFilter = getTop10;
+                break;
+            case 'bottom10':
+                currentFilter = getBottom10;
+                break;
+            case 'showAll':
+                currentFilter = getAllData;
+                break;
+        }
+        updateBarChart(document.getElementById('yearRange').value);
+    });
+});
+
+// Inicializar el gráfico con Top 10
+initializeChart();
+updateBarChart(document.getElementById('yearRange').value);
+
+// Escuchar cambios en el control deslizante de año
+document.getElementById('yearRange').addEventListener('input', function() {
+    updateBarChart(this.value);
+});
+
+// Actualizar el gráfico cuando cambie el tamaño de la ventana
+window.addEventListener('resize', function() {
+    updateBarChart(document.getElementById('yearRange').value);
+});
+
+
